df1c68b888830c85ce9f9100ed5da7a2730bcfa4
Marek Wiecek <marekw1986@gmail.com>
2026-03-22 21:21:15 +0100

    TIM4 OC initialization fix

--- code/Core/Src/main.c
+++ code/Core/Src/main.c
@@ -126,7 +126,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);   // HSYNC
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);   // VSYNC
   HAL_TIM_OC_Start_IT(&htim2, TIM_CHANNEL_2);
-  HAL_TIM_OC_Start_IT(&htim2, TIM_CHANNEL_3);
+  HAL_TIM_OC_Start_IT(&htim4, TIM_CHANNEL_3);
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
